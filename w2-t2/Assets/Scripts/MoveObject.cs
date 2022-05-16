25d376fb786903d690e1b6add1a6a1348fe03c32
kiet1994 <phanhoangkiet1994@gmail.com>
2022-05-16 21:21:28 +0700

    commit

---
+++ w2-t2/Assets/Scripts/MoveObject.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    public float Diff; // diff <0.2
+    public float MoveSpeed; 
+    private int currentIndex = 0; //
+
+    private Vector3[] points;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        points = PointsController.Instance.PosPoints;
+
+        if (currentIndex >= 0 && currentIndex < points.Length)
+        {
+            float distance = Vector3.Distance(points[currentIndex], transform.position);
+            if (distance < Diff)
+            {
+                if (currentIndex + 1 >= points.Length)
+                {
+                    currentIndex = 0;
+                }
+                else
+                {
+                    currentIndex++;
+                }
+            }
+            Vector3 dir = (points[currentIndex] - transform.position).normalized;
+            transform.Translate(dir * MoveSpeed * Time.deltaTime, Space.World);
+            
+        }
+    }
+}
